1f8e0ee82b13c2f32df1072590e8b551646a3ac0
IcaroJustino <icaromirandajustino@gmail.com>
2025-12-09 20:44:42 -0300

    feat:frontend-design

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,13 +1,34 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+// app.config.ts
+
+import {
+  ApplicationConfig,
+  importProvidersFrom,
+  provideBrowserGlobalErrorListeners,
+  provideZonelessChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+import { ReactiveFormsModule } from '@angular/forms';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
-  ]
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
+    provideAnimations(),
+    importProvidersFrom(ReactiveFormsModule),
+    importProvidersFrom(
+      ToastrModule.forRoot({
+        timeOut: 3000,
+        positionClass: 'toast-top-right',
+        preventDuplicates: true,
+        tapToDismiss: true,
+      })
+    ),
+  ],
 };
--- frontend/src/app/pages/login/login.ts
+++ frontend/src/app/pages/login/login.ts
@@ -1,15 +1,48 @@
-import { Component } from '@angular/core';
+// login.ts
+
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormControl, Validators, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ToastrModule, ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule, ToastrModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {
+export class Login implements OnInit {
   isPasswordVisible: boolean = false;
+
+  loginForm!: FormGroup;
+
+  constructor(private router: Router, private toastr: ToastrService) {}
+
+  ngOnInit(): void {
+    this.loginForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required]),
+    });
+  }
+
+  onsubmit(): void {
+    if (!this.loginForm.valid) {
+      this.toastr.warning('Por favor, preencha todos os campos corretamente.', 'Dados Incompletos');
+      console.log('Hello?');
+      return;
+    }
+    const email = this.loginForm.get('email')?.value;
+    const password = this.loginForm.get('password')?.value;
+    if (!(email === 'admin@email.com' && password === '123456')) {
+      this.toastr.error('E-mail ou senha inválidos. Tente novamente.', 'Acesso Negado');
+      return;
+    }
+    this.toastr.success('Login efetuado com sucesso!', 'Bem-vindo(a)!');
+    this.router.navigate(['/dashboard']);
+  }
 
   togglePasswordVisibility(): void {
     this.isPasswordVisible = !this.isPasswordVisible;
